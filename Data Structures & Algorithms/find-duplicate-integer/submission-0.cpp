639dbd732e50bad7cee43f4d26c24adff0df926d
SANDEEP KUMAR <145041098+sandeepkr0209@users.noreply.github.com>
2026-07-22 00:06:50 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (auto num : nums) {
+            freq[num]++;
+        }
+        for (auto num : nums) {
+            if (freq[num] > 1) return num;
+        }
+        return -1;
+    }
+};
