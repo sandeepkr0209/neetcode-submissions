ce82d49805166548a14be3f2342bd8568ee0c251
SANDEEP KUMAR <145041098+sandeepkr0209@users.noreply.github.com>
2026-08-01 15:38:09 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int i = 0;
+        while (i < nums.size() - 1) {
+            if (nums[i] == nums[i + 1]) {
+                i += 2;
+            } else {
+                return nums[i];
+            }
+        }
+        return nums[i];
+    }
+};
